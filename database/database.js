15c7485a6b7f4aa284b34d7aad0ae8664f7e4b6e
kuldeep patel <158982262+Devkuldeep@users.noreply.github.com>
2024-09-05 20:33:24 +0000

    DB Connected successfully

--- database/database.js
+++ database/database.js
@@ -1,13 +1,11 @@
-const { default: mongoose } = require("mongoose");
+const { mongoose } = require("mongoose");
 
-const MONGODB_URL =process.env;
-
-exports.connect=()=>{
-    mangoose.connect(MONGODB_URL,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    })
-    .then()
+const { MONGODB_URL } =process.env;
+console.log(typeof MONGODB_URL);
+exports.connect=()=>{   
+    
+    mongoose.connect(MONGODB_URL)
+    .then(console.log(`DB connected successfully`) )
     .catch((error)=>{
         console.log("DB connection failed");
         console.log(error);
