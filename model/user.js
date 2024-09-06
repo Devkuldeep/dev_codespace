d36702ddb89d5587d0bea73cee97d6a540f92c80
kuldeep patel <158982262+Devkuldeep@users.noreply.github.com>
2024-09-06 07:15:04 +0000

    rgistration and login setup

---
+++ model/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({ 
+    firstname:{
+        type: String,
+        default :null
+    },
+    lastname:{
+        type: String,
+        default :null
+    },
+    email:{
+        type: String,
+        unique: true
+    },
+    password:{
+        type: String,
+        
+    },
+    token:{
+        type: String,
+        default: null
+    },
+
+ });
+const user = mongoose.model('user', userSchema);
+
+module.exports =user;
